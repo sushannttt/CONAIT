988fe0ef63ea5cecfc290c16029b78269a3673c4
sushannttt <sushantjaiswal4991@gmail.com>
2025-07-21 13:17:24 +0530

    hopeless push 1

--- conait/build/web/flutter_service_worker.js
+++ conait/build/web/flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ebbcc65017996785c2de245cc6ac7666
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "56e21f249202136cd5974068c7ee4dbc",
+"flutter_bootstrap.js": "1ee5b8ed5c832390d334a1e626d0c702",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
